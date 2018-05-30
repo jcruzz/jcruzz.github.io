580a491d130a53aacb75e6c57d60f6dc387c28c9
Justin <jvcruz@ucdavis.edu>
2018-05-30 05:14:52 -0700

    add more projects

--- bundle.js
+++ bundle.js
@@ -18741,6 +18741,22 @@ exports.default = App;
 
 module.exports = [
   {
+    name: 'hangoutsidesf',
+    thumbnail: 'https://i.imgur.com/PUPfgrQm.png',
+    liveLink: null,
+    github: 'https://github.com/hangoutsidesf/hangoutsidesf',
+    language: 'Javascript',
+    stack: 'HTML, CSS, React, Express, Node',
+    info: `
+            I participated as a contributer in this team in charge of the front end
+            as well as implementing the UI/UX of the web application currently in development
+            I designed the UI using SASS and CSS Grids. This UI is friendly across
+            all devices including portrait and landscape orientations. This application
+            is designed as a Yelp-like website for parklets for those who are interested
+            in hanging out around the city of San Francisco.
+          `
+  },
+  {
   	name: 'Polling.io',
     thumbnail: 'https://i.imgur.com/u1zwc3I.png',
     liveLink: 'http://polling-io.herokuapp.com/',
@@ -18774,9 +18790,9 @@ module.exports = [
     language: 'Javascript',
     stack: 'Redis, Node/Express, Axios, shortid',
     info: `
-            This is a url-shortening microservice that uses Redis as the database for constant database lookups. 
-            The key of the database is the generated shortid (which uses the endpoint '/:id'), and the value that 
-            comes out is the website it shortened. The system design has two approaches, to create a shortened url, and to use a shortened url. 
+            This is a url-shortening microservice that uses Redis as the database for constant database lookups.
+            The key of the database is the generated shortid (which uses the endpoint '/:id'), and the value that
+            comes out is the website it shortened. The system design has two approaches, to create a shortened url, and to use a shortened url.
           `
   },
   {
@@ -18787,8 +18803,8 @@ module.exports = [
     language: 'Javascript',
     stack: 'D3.js, HTML, CSS',
     info: `
-            This is a heat map created to measure the land surface temperature from 1753 to 2015. 
-            The graph is populated by bars that correspends to the years and months respectively 
+            This is a heat map created to measure the land surface temperature from 1753 to 2015.
+            The graph is populated by bars that correspends to the years and months respectively
             located at the X and Y axis, and using the color to determine the temperature. More
             information can be shown by hoving over the bar.
           `
@@ -18805,16 +18821,60 @@ module.exports = [
             future projects that require authentication. It's also doubles as a React-Router boilerplate.
             Future developments will include styling, making a different version with a different database,
             and include social media authentication in order to hit the ground running when developing
-            a new project. 
+            a new project.
           `
   },
 ];
+
 
 /***/ }),
 /* 30 */
 /***/ (function(module, exports) {
 
 module.exports = [
+	{
+		name: 'hangoutsidesf',
+		thumbnail: 'https://i.imgur.com/PUPfgrQm.png',
+		liveLink: null,
+		github: 'https://github.com/hangoutsidesf/hangoutsidesf',
+		language: 'Javascript',
+		stack: 'HTML, CSS, React, Express, Node',
+		info: `
+						I participated as a contributer in this team in charge of the front end
+						as well as implementing the UI/UX of the web application currently in development
+						I designed the UI using SASS and CSS Grids. This UI is friendly across
+						all devices including portrait and landscape orientations. This application
+						is designed as a Yelp-like website for parklets for those who are interested
+						in hanging out around the city of San Francisco.
+					`
+	},
+	{
+		name: 'v-pedia',
+		thumbnail: 'https://i.imgur.com/2Vv6nzMm.png',
+		liveLink: 'https://v-pedia.herokuapp.com/',
+		github: 'https://github.com/jcruzz/v-pedia',
+		language: 'Javascript',
+		stack: 'HTML, CSS, Vue JS 2, Express, Node',
+		info: `
+						This is a search engine that utilizes the Wikipedia API to display
+						query results that includes the description and link to the article.
+						Some features include a random button similar to Google's 'I'm feeling
+						lucky' function.
+		      `
+	},
+	{
+		name: 'Twitch User Tracker',
+		thumbnail: 'https://i.imgur.com/b0WmOyJm.png',
+		liveLink: 'http://twitch-tracker.herokuapp.com/',
+		github: 'https://github.com/jcruzz/twitch-user-tracker',
+		language: 'Javascript',
+		stack: 'HTML, CSS, Vue JS 2, Express, Node',
+		info: `
+						This is a tracker that uses the Twitch API to see user activity. Will
+						provide information on online/offline, status message, profile icon, and
+						current game being streamed along with the URL to the stream.
+		      `
+	},
 	{
 	  	name: 'Weather App',
 	    thumbnail: 'https://i.imgur.com/QzgNZfA.png',
@@ -18823,10 +18883,10 @@ module.exports = [
 	    language: 'Javascript',
 	    stack: 'HTML, CSS',
 	    info: `
-							An application I created in August 2017 that uses the OpenWeatherMap 
-							Api to generate the Local Weather. The interface comes with a switch 
-							where you can switch between Celsius and Fahrenheit. Possibly due to 
-							the time I've created this, the image links to help generate the 
+							An application I created in August 2017 that uses the OpenWeatherMap
+							Api to generate the Local Weather. The interface comes with a switch
+							where you can switch between Celsius and Fahrenheit. Possibly due to
+							the time I've created this, the image links to help generate the
 							weather visual expired.
 	          `
 	},
@@ -18838,9 +18898,9 @@ module.exports = [
 	    language: 'Javascript',
 	    stack: 'HTML, CSS',
 	    info: `
-	            A Project I did a last year.  This front end project utilizes basic 
-	            Javascript in generating random quotes. The quotes generated can be 
-	            tweeted using a convenient button that grabs whatever generated quote 
+	            A Project I did a last year.  This front end project utilizes basic
+	            Javascript in generating random quotes. The quotes generated can be
+	            tweeted using a convenient button that grabs whatever generated quote
 	            and opens up a new window.
 	          `
 	},
@@ -18852,12 +18912,13 @@ module.exports = [
 	    language: 'Javascript',
 	    stack: 'HTML, CSS',
 	    info: `
-	            My first completed page that started my journey in the world of 
-	            development. This is a simple tribute page for an overlooked legend in 
+	            My first completed page that started my journey in the world of
+	            development. This is a simple tribute page for an overlooked legend in
 	            Scottie Pippen.
 	          `
 	},
 ];
+
 
 /***/ }),
 /* 31 */
@@ -19093,6 +19154,30 @@ module.exports = [
 
 module.exports = [
   {
+    name: 'Massport',
+    thumbnail: null,
+    liveLink: null,
+    github: 'https://github.com/jcruzz/Massport',
+    language: 'Javascript, Mongo',
+    stack: 'Node/Express, React, React-Router, Passport (local, github, google, facebook, twitter)',
+    info: `
+            An authentication suite that incorporates multiple strategies to be used as a boilerplate for future projects.
+          `
+  },
+  {
+    name: 'Vue CLI Full Stack Boilerplate',
+    thumbnail: null,
+    liveLink: null,
+    github: 'https://github.com/jcruzz/vue-full-stack-starter',
+    language: 'Javascript',
+    stack: 'Vue, Node/Express',
+    info: `
+            This is a small boiletplate that uses the Vue CLI, but instead of just a dev server, a regular
+            node server was created through the use of a proxy in conjunction with the dev server in order
+            to make a full stack application instead of the exclusive front end the Vue CLI provides.
+          `
+  },
+  {
     name: 'JWT-Auth React Boilerplate',
     thumbnail: null,
     liveLink: 'https://frozen-depths-11875.herokuapp.com/',
@@ -19104,7 +19189,7 @@ module.exports = [
             future projects that require authentication. It's also doubles as a React-Router boilerplate.
             Future developments will include styling, making a different version with a different database,
             and include social media authentication in order to hit the ground running when developing
-            a new project. 
+            a new project.
           `
   },
   {
@@ -19130,6 +19215,7 @@ module.exports = [
           `
   },
 ];
+
 
 /***/ }),
 /* 35 */
